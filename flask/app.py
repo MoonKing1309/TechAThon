05bd66384a3d0d06b26c5a8cb6f027b64dd5e806
MoonKing1309 <conorken1@gmail.com>
2024-04-21 00:37:04 +0530

    WorkingModel

--- flask/app.py
+++ flask/app.py
@@ -16,18 +16,21 @@ def hello_world():
     return render_template("index.html")
 
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predictLR',methods=['POST','GET'])
 def predict():
-    text =''
+    i =''
     for key, value in request.form.items():
         if key == 'text':  # Replace 'input1', 'input2' with your desired input keys
-            text = value
+            i = value
+    text =[]
+    text.append(i)
     textdata = vectoriser.transform(preprocess(text))
     sentiment = model.predict(textdata)
-    if sentiment==0:
-        return render_template('index.html',pred='The Sentiment of the given text is {}'.format("Negative"))
+
+    if sentiment[0]==0:
+        return render_template('index.html',pred='The Sentiment of the given text is Negative')
     else:
-        return render_template('index.html',pred='The Sentiment of the given text is {}'.format("Positive"))
+        return render_template('index.html',pred='The Sentiment of the given text is Positive')
 
 
 
